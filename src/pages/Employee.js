d624b02644c979b10117bc0255820dc27f1b6a29
Ash <aashraya_p@hotmail.com>
2023-10-03 19:04:27 -0500

    employee loading

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -181,15 +181,15 @@ function Employee() {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            zIndex: 10,  // Ensure the loader is on top
+            zIndex: 10,
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
             width: '100%',
             height: '100%',
-            backgroundColor: '#5d23944b'  // Optional: This provides a semi-transparent white background
+            backgroundColor: '#5d23944b'
           }}>
-            <CircularProgress size={50} />  {/* Adjust size as needed */}
+            <CircularProgress size={100} />
           </div>
         )}
         <StyledTable>
