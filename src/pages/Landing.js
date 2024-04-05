9b44b945300b36ce5a2f99c4de8b4b5dcbab07d2
chandanInTech <kurella.chandu4@gmail.com>
2024-04-05 02:05:55 -0500

    Error Email pass error

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,129 +1,141 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
-import { FormField, LoginButton, Wrapper, Content, LoginCard, Logo } from '../styles/styledLanding';
-import { isValidEmail } from '../utils/fieldUtil';
+import {CircularProgress} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+import {AuthContext} from '../utils/AuthProvider'; // Ensure this path matches where AuthProvider is located
+import {isValidEmail} from '../utils/fieldUtil';
+import {FormField, LoginButton, Wrapper, Content, LoginCard, Logo} from '../styles/styledLanding';
 import logo from '../assets/images/logochandan2.png';
-import { AuthContext } from '../utils/AuthProvider';
-import { useNavigate } from 'react-router-dom';
-import { CircularProgress } from '@mui/material';
 
-const LoginForm = ({ email, setEmail, password, setPassword, handleSubmit, submitted, loading, emailError }) => (
+const LoginForm = ({
+                       email,
+                       setEmail,
+                       password,
+                       setPassword,
+                       handleSubmit,
+                       submitted,
+                       loading,
+                       authError, // Rename this prop to match the error coming from AuthContext
+                   }) => (
     <>
-      <Typography component="h1" variant="h5">
-        Sign In
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <FormField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            autoComplete="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            error={submitted && (!isValidEmail(email) || emailError)}
-            helperText={submitted && (emailError || (!isValidEmail(email) ? 'Invalid Email' : ''))}
-        />
-        <FormField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="password"
-            label="Password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-        />
-        <LoginButton
-            type="submit"
-            fullWidth
-            variant="contained"
-            disabled={loading}
-        >
-          {loading ? 'Logging in...' : 'Sign-In'}
-        </LoginButton>
-      </form>
+        <Typography component="h1" variant="h5">
+            Sign In
+        </Typography>
+        <form onSubmit={handleSubmit}>
+            <FormField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={submitted && (!isValidEmail(email) || authError)}
+                helperText={submitted && ((!isValidEmail(email) ? 'Invalid Email' : ''))}
+            />
+            <FormField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            {submitted && authError && (
+                <Typography color="error" style={{marginTop: 8}}>
+                    {authError}
+                </Typography>
+            )}
+            <LoginButton
+                type="submit"
+                fullWidth
+                variant="contained"
+                disabled={loading}
+            >
+                {loading ? 'Logging in...' : 'Sign In'}
+            </LoginButton>
+        </form>
     </>
 );
 
 LoginForm.propTypes = {
-  email: PropTypes.string.isRequired,
-  setEmail: PropTypes.func.isRequired,
-  password: PropTypes.string.isRequired,
-  setPassword: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  submitted: PropTypes.bool.isRequired,
-  loading: PropTypes.bool.isRequired,
-  emailError: PropTypes.string
+    email: PropTypes.string.isRequired,
+    setEmail: PropTypes.func.isRequired,
+    password: PropTypes.string.isRequired,
+    setPassword: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    submitted: PropTypes.bool.isRequired,
+    loading: PropTypes.bool.isRequired,
+    authError: PropTypes.string, // Update the propType definition
 };
 
 const Landing = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-  const [emailError, setEmailError] = useState('');
-  const { signInWithEmailAndPassword, loading: authLoading, currentUser, initializationCompleted } = useContext(AuthContext);
-  const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+    const {
+        signInWithEmailAndPassword,
+        loading: authLoading,
+        currentUser,
+        initializationCompleted,
+        error: authError
+    } = useContext(AuthContext);
+    const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setSubmitted(true);
-    if (!isValidEmail(email)) {
-      setEmailError('Invalid Email');
-      return;
-    }
-    setEmailError('');
-    try {
-      await signInWithEmailAndPassword(email, password);
-      // Navigation to '/company' or handling of the login success is done within useEffect based on currentUser change
-    } catch (error) {
-      setEmailError(error.message); // Assuming signInWithEmailAndPassword function throws or passes error messages appropriately
-    }
-  };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setSubmitted(true);
+        if (!isValidEmail(email)) {
+            return; // The error handling for invalid email is already being done in LoginForm, so no need to duplicate here.
+        }
+        await signInWithEmailAndPassword(email, password);
+    };
 
-  useEffect(() => {
-    if (initializationCompleted && currentUser) {
-      navigate('/company');
-    }
-  }, [initializationCompleted, currentUser, navigate]);
+    useEffect(() => {
+        if (initializationCompleted && currentUser) {
+            navigate('/company');
+        }
+    }, [initializationCompleted, currentUser, navigate]);
 
-  if (!initializationCompleted || authLoading) {
+    if (!initializationCompleted || authLoading) {
+        return (
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+                <CircularProgress/>
+            </div>
+        );
+    }
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-          <CircularProgress />
-        </div>
+        <Wrapper>
+            <Content>
+                <LoginCard>
+                    <CardContent>
+                        <Logo src={logo} alt="Logo"/>
+                        <LoginForm
+                            email={email}
+                            setEmail={setEmail}
+                            password={password}
+                            setPassword={setPassword}
+                            handleSubmit={handleSubmit}
+                            submitted={submitted}
+                            loading={authLoading}
+                            authError={authError} // Pass the error from AuthContext to LoginForm
+                        />
+                    </CardContent>
+                </LoginCard>
+            </Content>
+        </Wrapper>
     );
-  }
-
-  return (
-      <Wrapper>
-        <Content>
-          <LoginCard>
-            <CardContent>
-              <Logo src={logo} alt="Logo" />
-              <LoginForm
-                  email={email}
-                  setEmail={setEmail}
-                  password={password}
-                  setPassword={setPassword}
-                  handleSubmit={handleSubmit}
-                  submitted={submitted}
-                  loading={authLoading}
-                  emailError={emailError}
-              />
-            </CardContent>
-          </LoginCard>
-        </Content>
-      </Wrapper>
-  );
 };
 
 export default Landing;
--- src/utils/AuthProvider.js
+++ src/utils/AuthProvider.js
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
       const adminDetails = await fetchAdminByEmail(email);
       setCurrentAdmin(adminDetails);
     } catch (error) {
-      setError(error.message);
+      setError('Invalid Email or Password. Please try again.');
     } finally {
       setLoading(false);
     }
