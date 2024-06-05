05ca336bec5844e4b6a91e8009f34d862838e6e5
Ash <aashraya_p@hotmail.com>
2024-06-05 17:45:05 -0500

    bulk upload getidtoken fix

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,166 +1,166 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
-import {CircularProgress, Link} from '@mui/material';
-import {useNavigate} from 'react-router-dom';
-import {AuthContext} from '../utils/AuthProvider'; // Ensure this path matches where AuthProvider is located
-import {isValidEmail} from '../utils/fieldUtil';
-import {FormField, LoginButton, Wrapper, Content, LoginCard, Logo} from '../styles/styledLanding';
+import { CircularProgress, Link } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../utils/AuthProvider'; // Ensure this path matches where AuthProvider is located
+import { isValidEmail } from '../utils/fieldUtil';
+import { FormField, LoginButton, Wrapper, Content, LoginCard, Logo } from '../styles/styledLanding';
 import logo from '../assets/images/logochandan2.png';
 
 const LoginForm = ({
-                       email,
-                       setEmail,
-                       password,
-                       setPassword,
-                       handleSubmit,
-                       submitted,
-                       loading,
-                       authError,
-                       onForgotPassword, // Include the onForgotPassword function
-                   }) => (
-    <>
-        <Typography component="h1" variant="h5">
+  email,
+  setEmail,
+  password,
+  setPassword,
+  handleSubmit,
+  submitted,
+  loading,
+  authError,
+  onForgotPassword, // Include the onForgotPassword function
+}) => (
+  <>
+    <Typography component="h1" variant="h5">
             Sign In
+    </Typography>
+    <form onSubmit={handleSubmit}>
+      <FormField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email"
+        name="email"
+        autoComplete="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        error={submitted && (!isValidEmail(email) || authError)}
+        helperText={submitted && ((!isValidEmail(email) ? 'Invalid Email' : ''))}
+      />
+      <FormField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="password"
+        label="Password"
+        name="password"
+        type="password"
+        autoComplete="current-password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {submitted && authError && (
+        <Typography color="error" style={{ marginTop: 8 }}>
+          {authError}
         </Typography>
-        <form onSubmit={handleSubmit}>
-            <FormField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                error={submitted && (!isValidEmail(email) || authError)}
-                helperText={submitted && ((!isValidEmail(email) ? 'Invalid Email' : ''))}
-            />
-            <FormField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            {submitted && authError && (
-                <Typography color="error" style={{marginTop: 8}}>
-                    {authError}
-                </Typography>
-            )}
-            <LoginButton
-                type="submit"
-                fullWidth
-                variant="contained"
-                disabled={loading}
-            >
-                {loading ? 'Logging in...' : 'Sign In'}
-            </LoginButton>
-            <Typography align="center" style={{marginTop: 16}}>
-                <Link
-                    component="button"
-                    variant="body2"
-                    onClick={onForgotPassword} // Call the onForgotPassword function when clicked
-                >
+      )}
+      <LoginButton
+        type="submit"
+        fullWidth
+        variant="contained"
+        disabled={loading}
+      >
+        {loading ? 'Logging in...' : 'Sign In'}
+      </LoginButton>
+      <Typography align="center" style={{ marginTop: 16 }}>
+        <Link
+          component="button"
+          variant="body2"
+          onClick={onForgotPassword} // Call the onForgotPassword function when clicked
+        >
                     Forgot Password?
-                </Link>
-            </Typography>
-        </form>
-    </>
+        </Link>
+      </Typography>
+    </form>
+  </>
 );
 
 LoginForm.propTypes = {
-    email: PropTypes.string.isRequired,
-    setEmail: PropTypes.func.isRequired,
-    password: PropTypes.string.isRequired,
-    setPassword: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    submitted: PropTypes.bool.isRequired,
-    loading: PropTypes.bool.isRequired,
-    authError: PropTypes.string,
-    onForgotPassword: PropTypes.func.isRequired, // Add propType for onForgotPassword
+  email: PropTypes.string.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  password: PropTypes.string.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  submitted: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
+  authError: PropTypes.string,
+  onForgotPassword: PropTypes.func.isRequired, // Add propType for onForgotPassword
 };
 
 const Landing = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [submitted, setSubmitted] = useState(false);
-    const {
-        signInWithEmailAndPassword,
-        loading: authLoading,
-        currentUser,
-        initializationCompleted,
-        error: authError,
-        resetPassword, // Destructure the resetPassword function from AuthContext
-    } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const {
+    signInWithEmailAndPassword,
+    loading: authLoading,
+    currentUser,
+    initializationCompleted,
+    error: authError,
+    resetPassword, // Destructure the resetPassword function from AuthContext
+  } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setSubmitted(true);
-        if (!isValidEmail(email)) {
-            return;
-        }
-        await signInWithEmailAndPassword(email, password);
-    };
-
-    useEffect(() => {
-        if (initializationCompleted && currentUser) {
-            navigate('/company');
-        }
-    }, [initializationCompleted, currentUser, navigate]);
-
-    // Function to handle forgot password action
-    const handleForgotPassword = () => {
-        const userEmail = window.prompt('Please enter your email address:');
-        if (userEmail) {
-            resetPassword(userEmail).then(() => {
-                alert('If there is an account associated with that email, a reset link has been sent.');
-            }).catch((error) => {
-                alert('Failed to send reset email. Please try again.');
-            });
-        }
-    };
-
-    if (!initializationCompleted || authLoading) {
-        return (
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-                <CircularProgress/>
-            </div>
-        );
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+    if (!isValidEmail(email)) {
+      return;
     }
+    await signInWithEmailAndPassword(email, password);
+  };
 
+  useEffect(() => {
+    if (initializationCompleted && currentUser) {
+      navigate('/company');
+    }
+  }, [initializationCompleted, currentUser, navigate]);
+
+  // Function to handle forgot password action
+  const handleForgotPassword = () => {
+    const userEmail = window.prompt('Please enter your email address:');
+    if (userEmail) {
+      resetPassword(userEmail).then(() => {
+        alert('If there is an account associated with that email, a reset link has been sent.');
+      }).catch((error) => {
+        alert('Failed to send reset email. Please try again.');
+      });
+    }
+  };
+
+  if (!initializationCompleted || authLoading) {
     return (
-        <Wrapper>
-            <Content>
-                <LoginCard>
-                    <CardContent>
-                        <Logo src={logo} alt="Logo"/>
-                        <LoginForm
-                            email={email}
-                            setEmail={setEmail}
-                            password={password}
-                            setPassword={setPassword}
-                            handleSubmit={handleSubmit}
-                            submitted={submitted}
-                            loading={authLoading}
-                            authError={authError}
-                            onForgotPassword={handleForgotPassword} // Pass the forgot password handler
-                        />
-                    </CardContent>
-                </LoginCard>
-            </Content>
-        </Wrapper>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress />
+      </div>
     );
+  }
+
+  return (
+    <Wrapper>
+      <Content>
+        <LoginCard>
+          <CardContent>
+            <Logo src={logo} alt="Logo" />
+            <LoginForm
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+              handleSubmit={handleSubmit}
+              submitted={submitted}
+              loading={authLoading}
+              authError={authError}
+              onForgotPassword={handleForgotPassword} // Pass the forgot password handler
+            />
+          </CardContent>
+        </LoginCard>
+      </Content>
+    </Wrapper>
+  );
 };
 
 export default Landing;
--- src/utils/AuthProvider.js
+++ src/utils/AuthProvider.js
@@ -8,93 +8,107 @@ const auth = firebaseAuth;
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [currentAdmin, setCurrentAdmin] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [initializationCompleted, setInitializationCompleted] = useState(false);
-    const [error, setError] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [currentAdmin, setCurrentAdmin] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [initializationCompleted, setInitializationCompleted] = useState(false);
+  const [error, setError] = useState(null);
 
-    // Function to handle signing in with email and password
-    const signInWithEmailAndPasswordHandler = async (email, password) => {
+  // Function to handle signing in with email and password
+  const signInWithEmailAndPasswordHandler = async (email, password) => {
+    try {
+      setLoading(true);
+      setError(null);
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      setCurrentUser(result.user);
+      const adminDetails = await fetchAdminByEmail(email);
+      setCurrentAdmin(adminDetails);
+    } catch (error) {
+      setError('Invalid Email or Password. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getIdToken = async () => {
+    console.log(currentUser,'currentuser');
+    if (!currentUser) {
+      return null;
+    }
+    try {
+      return await currentUser.getIdToken(true);
+    } catch (error) {
+      setError(error.message);
+      return null;
+    }
+  };
+
+  // Function to handle signing out
+  const signOut = async () => {
+    setLoading(true);
+    try {
+      await firebaseSignOut(auth);
+      setCurrentUser(null);
+      setCurrentAdmin(null);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Function to handle password reset
+  const resetPassword = async (email) => {
+    try {
+      setLoading(true);
+      await sendPasswordResetEmail(auth, email);
+      setError(null); // Optionally, set a success message instead of nullifying the error
+    } catch (error) {
+      setError('Failed to send password reset email. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      setCurrentUser(user);
+      if (user) {
         try {
-            setLoading(true);
-            setError(null);
-            const result = await signInWithEmailAndPassword(auth, email, password);
-            setCurrentUser(result.user);
-            const adminDetails = await fetchAdminByEmail(email);
-            setCurrentAdmin(adminDetails);
+          const adminDetails = await fetchAdminByEmail(user.email);
+          setCurrentAdmin(adminDetails);
         } catch (error) {
-            setError('Invalid Email or Password. Please try again.');
-        } finally {
-            setLoading(false);
+          setError(error.message);
         }
-    };
+      } else {
+        setCurrentAdmin(null);
+      }
+      setLoading(false);
+      setInitializationCompleted(true);
+    });
 
-    // Function to handle signing out
-    const signOut = async () => {
-        setLoading(true);
-        try {
-            await firebaseSignOut(auth);
-            setCurrentUser(null);
-            setCurrentAdmin(null);
-        } catch (error) {
-            setError(error.message);
-        } finally {
-            setLoading(false);
-        }
-    };
+    return () => unsubscribe();
+  }, []);
 
-    // Function to handle password reset
-    const resetPassword = async (email) => {
-        try {
-            setLoading(true);
-            await sendPasswordResetEmail(auth, email);
-            setError(null); // Optionally, set a success message instead of nullifying the error
-        } catch (error) {
-            setError('Failed to send password reset email. Please try again.');
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(async (user) => {
-            setCurrentUser(user);
-            if (user) {
-                try {
-                    const adminDetails = await fetchAdminByEmail(user.email);
-                    setCurrentAdmin(adminDetails);
-                } catch (error) {
-                    setError(error.message);
-                }
-            } else {
-                setCurrentAdmin(null);
-            }
-            setLoading(false);
-            setInitializationCompleted(true);
-        });
-
-        return () => unsubscribe();
-    }, []);
-
-    return (
-        <AuthContext.Provider value={{
-            currentUser,
-            currentAdmin,
-            error,
-            loading,
-            initializationCompleted,
-            signInWithEmailAndPassword: signInWithEmailAndPasswordHandler,
-            signOut,
-            resetPassword, // Add this line
-        }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{
+      currentUser,
+      currentAdmin,
+      error,
+      loading,
+      initializationCompleted,
+      signInWithEmailAndPassword: signInWithEmailAndPasswordHandler,
+      signOut,
+      getIdToken,
+      resetPassword,
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 AuthProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthProvider;
